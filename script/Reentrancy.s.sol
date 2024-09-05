30d0ea773e4cbc17d848e4e1985e0363201174a2
Roberto Cantu <Roberto_Cantu@live.com>
2024-09-05 16:29:35 -0500

    Update deployment script for demo

--- script/Reentrancy.s.sol
+++ script/Reentrancy.s.sol
@@ -20,9 +20,18 @@ contract Reentrancy is Script {
         // Set up `InternalFirewall` constructor args
         AttestationForwarder _trustedForwarder = new AttestationForwarder();
         SecurityValidator securityValidator = new SecurityValidator(address(_trustedForwarder));
-        bytes32 attesterControllerId = bytes32(uint256(1));                             // Id of Attester Controller deployed for reentrancy demo
-        address defaultAdmin = 0xC99E8AB127272119a42e30A88087b0DaA4807aDA;              // Corresponds to `VICTIM_PRIVATE_KEY`
+        // Id of Attester Controller deployed for reentrancy demo
+        bytes32 attesterControllerId = bytes32(uint256(1));
+        // Corresponds to `VICTIM_PRIVATE_KEY`
+        address defaultAdmin = 0xC99E8AB127272119a42e30A88087b0DaA4807aDA;
         FirewallAccess firewallAccess = new FirewallAccess(defaultAdmin);
+
+        // Set up access controls to properly execute benign and exploit txns
+        firewallAccess.grantRole(FIREWALL_ADMIN_ROLE, defaultAdmin);
+        firewallAccess.grantRole(PROTOCOL_ADMIN_ROLE, defaultAdmin);
+        firewallAccess.grantRole(ATTESTER_MANAGER_ROLE, defaultAdmin);
+        address trustedAttester = 0x875A57917E46A440c71d9EC6F6B5c8B772D6C895;
+        firewallAccess.grantRole(TRUSTED_ATTESTER_ROLE, trustedAttester);
 
         ReentrancyVulnerable reentrancyVulnerable = new ReentrancyVulnerable(
             ISecurityValidator(address(securityValidator)),
@@ -44,15 +53,14 @@ contract Reentrancy is Script {
         });
         reentrancyVulnerable.setCheckpoint(funcSelector, checkpoint);
 
-        // reentrancyVulnerable.deposit{value: 5 ether}();
-
 
 
 
         // uint256 attackerPrivateKey = vm.envUint("ATTACKER_PRIVATE_KEY");
         // vm.startBroadcast(attackerPrivateKey);
 
-        // address reentrancyVulnerableAddress = 0xdEADBEeF00000000000000000000000000000000; // TODO: Add address of `ReentrancyVulnerable` contract
+        // // Should be set to address of `ReentrancyVulnerable` contract deployed at L33-37
+        // address reentrancyVulnerableAddress = 0x38ac925656DA89fc667f5F3Aa4e8417571AB73fB;
         // ReentrancyAttack reentrancyAttack = new ReentrancyAttack(reentrancyVulnerableAddress);
 
 
