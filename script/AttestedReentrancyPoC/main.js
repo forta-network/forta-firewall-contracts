7712306f1e49fcc89463771ebef301e291f8dce6
Roberto Cantu <Roberto_Cantu@live.com>
2024-09-10 13:58:28 -0500

    Create 'main' script to calls the separated sub-scripts

---
+++ script/AttestedReentrancyPoC/main.js
@@ -0,0 +1,27 @@
+const {
+  executeBenignDepositAndWithdraw
+} = require('./executeBenignDepositAndWithdraw');
+const {
+  executeMaliciousReentrancy
+} = require('./executeMaliciousReentrancy');
+
+// Booleans to decide which transaction flow
+// to be executed: benign and/or malicious
+const benignDepositAndWithdraw = true;
+const maliciousReentrancy = true;
+
+async function main() {
+    if(benignDepositAndWithdraw) {
+      console.log("\n\nExecuting benign deposit and withdrawal from" +
+      "'ReentrancyAttack' contract to 'ReentrancyVulnerable' contract.\n\n");
+      await executeBenignDepositAndWithdraw();
+    }
+
+    if(maliciousReentrancy) {
+      console.log("\n\nExecuting malicious reentrancy attack from" +
+      "'ReentrancyAttack' contract to 'ReentrancyVulnerable' contract.\n\n");
+      await executeMaliciousReentrancy();
+    }
+  }
+
+main();
